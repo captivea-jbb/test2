098ce9a8093a462cc23dd0823b9ed8647c570cc1
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2021-05-18 17:15:18 -0400

    Adding modules

---
+++ cap_ecom_downpayment/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Downpayment on Website",
+
+    'summary': """
+        Charges only 25 percent of a product's price on the website.
+        """,
+
+    'author': "Captivea",
+    'website': "https://www.Captivea.us/",
+    'license': 'LGPL-3',
+
+    'category': 'Website',
+    'version': '1.0',
+
+    'depends': ['base','website','website_sale'],
+
+    'data': [],
+
+}
